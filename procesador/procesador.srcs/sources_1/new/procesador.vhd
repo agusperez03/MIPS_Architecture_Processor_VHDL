7fcf039d64d2e54b06e6ea8b32ada95d6f7d2b57
Agustín Crespo <acrespo@alumnos.exa.unicen.edu.ar>
2023-06-26 07:46:07 -0300

    Prueba Individual (posible entrega)

--- procesador/procesador.srcs/sources_1/new/procesador.vhd
+++ procesador/procesador.srcs/sources_1/new/procesador.vhd
@@ -61,23 +61,24 @@ architecture processor_arq of processor is
 
 
 -- senales de la Unidad de Control
-	signal RegDst: std_logic_vector;
-	signal ALUSrc: std_logic_vector;
-	signal MemtoReg: std_logic_vector;
-	signal RegWrite: std_logic_vector;
-	signal MemRead: std_logic_vector;
-	signal MemWrite: std_logic_vector;
-	signal Branch: std_logic_vector;
+	signal RegDst: std_logic;
+	signal ALUSrc: std_logic;
+	signal MemtoReg: std_logic;
+	signal RegWrite: std_logic;
+	signal MemRead: std_logic;
+	signal MemWrite: std_logic;
+	signal Branch: std_logic;
 	signal ALUOp: std_logic_vector(2 downto 0);
 
 
 -- senales de registro de segmentacion ID/EX
-	signal ID_RegDst: std_logic_vector;
-	signal ID_ALUSrc: std_logic_vector;
-	signal ID_MemtoReg: std_logic_vector;
-	signal ID_MemRead: std_logic_vector;
-	signal ID_MemWrite: std_logic_vector;
-	signal ID_Branch: std_logic_vector;
+	signal ID_RegDst: std_logic;
+    signal ID_RegWrite: std_logic;
+	signal ID_ALUSrc: std_logic;
+	signal ID_MemtoReg: std_logic;
+	signal ID_MemRead: std_logic;
+	signal ID_MemWrite: std_logic;
+	signal ID_Branch: std_logic;
 	signal ID_ALUOp: std_logic_vector(2 downto 0);
 	signal ID_pc_plus_4: std_logic_vector(31 downto 0);
 	signal ID_read_data_1: std_logic_vector(31 downto 0);
@@ -93,7 +94,7 @@ architecture processor_arq of processor is
 	signal zero_ALU: std_logic;
 	signal result_ALU: std_logic_vector(31 downto 0);
 	signal mux_instruction: std_logic_vector(4 downto 0);
-	signal add_jump: std_logic_vector(31 downto 0);
+	signal EX_pc_branch: std_logic_vector(31 downto 0);
 
 	Component ALU 
 		Port ( a : in STD_LOGIC_VECTOR (31 downto 0);
@@ -103,11 +104,12 @@ architecture processor_arq of processor is
 			zero : out STD_LOGIC);
 	end component;
 	
--- se�ales del registro de segmentacion EX/MEM
-	signal EX_MemtoReg: std_logic_vector;
-	signal EX_MemRead: std_logic_vector;
-	signal EX_MemWrite: std_logic_vector;
-	signal EX_Branch: std_logic_vector;
+-- se?ales del registro de segmentacion EX/MEM
+    signal EX_RegWrite: std_logic;
+	signal EX_MemtoReg: std_logic;
+	signal EX_MemRead: std_logic;
+	signal EX_MemWrite: std_logic;
+	signal EX_Branch: std_logic;
 	signal EX_zero_ALU: std_logic;
 	signal EX_result_ALU: std_logic_vector(31 downto 0);
 	signal EX_mux_instruction: std_logic_vector(4 downto 0);
@@ -116,9 +118,13 @@ architecture processor_arq of processor is
 
 
 -- Etapa de WB
-	signal WB_write_register: std_logic_vector(4 downto 0);
-	signal WB_write_data: std_logic_vector(31 downto 0);
-	signal WB_write: std_logic;
+	signal WB_write_register: std_logic_vector(4 downto 0); -- instruction
+	signal WB_write_data: std_logic_vector(31 downto 0); -- salida del mux
+    signal WB_result_data_memory: std_logic_vector(31 downto 0);
+    signal WB_RegWrite: std_logic;
+    signal WB_MemtoReg: std_logic;
+    signal WB_read_data_2: std_logic_vector(31 downto 0);
+ 
 
 
 begin 	
@@ -165,7 +171,7 @@ begin
 	port map(
 		clk => Clk,
 		reset => Reset,
-		wr => WB_write,
+		wr => WB_RegWrite,
 		reg1_rd => if_instruction(25 downto 21),
 		reg2_rd => if_instruction(20 downto 16),
 		reg_wr => WB_write_register, 
@@ -179,85 +185,85 @@ begin
 	begin
 		case if_instruction(31 downto 26) is
 			when "000000" => -- r-type
-				RegDst <= "1";
-				ALUSrc <= "0";
-				MemtoReg <= "0";
-				RegWrite <= "1";
-				MemRead <= "0";
-				MemWrite <= "0";
-				Branch <= "0";
+				RegDst <= '1';
+				ALUSrc <= '0';
+				MemtoReg <= '0';
+				RegWrite <= '1';
+				MemRead <= '0';
+				MemWrite <= '0';
+				Branch <= '0';
 				ALUOp <= "010";
 			when "100011" => -- lw
-				RegDst <= "0";
-				ALUSrc <= "1";
-				MemtoReg <= "1";
-				RegWrite <= "1";
-				MemRead <= "1";
-				MemWrite <= "0";
-				Branch <= "0";
+				RegDst <= '0';
+				ALUSrc <= '1';
+				MemtoReg <= '1';
+				RegWrite <= '1';
+				MemRead <= '1';
+				MemWrite <= '0';
+				Branch <= '0';
 				ALUOp <= "000";
 			when "101011" => -- sw
-				RegDst <= "0";
-				ALUSrc <= "1";
-				MemtoReg <= "0";
-				RegWrite <= "0";
-				MemRead <= "0";
-				MemWrite <= "1";
-				Branch <= "0";
+				RegDst <= '0';
+				ALUSrc <= '1';
+				MemtoReg <= '0';
+				RegWrite <= '0';
+				MemRead <= '0';
+				MemWrite <= '1';
+				Branch <= '0';
 				ALUOp <= "000";
 			when "000100" => -- beq
-				RegDst <= "0";
-				ALUSrc <= "0";
-				MemtoReg <= "0";
-				RegWrite <= "0";
-				MemRead <= "0";
-				MemWrite <= "0";
-				Branch <= "1";
+				RegDst <= '0';
+				ALUSrc <= '0';
+				MemtoReg <= '0';
+				RegWrite <= '0';
+				MemRead <= '0';
+				MemWrite <= '0';
+				Branch <= '1';
 				ALUOp <= "001";
 			when "001000" => -- addi
-				RegDst <= "0";
-				ALUSrc <= "1";
-				MemtoReg <= "0";
-				RegWrite <= "1";
-				MemRead <= "0";
-				MemWrite <= "0";
-				Branch <= "0";
+				RegDst <= '0';
+				ALUSrc <= '1';
+				MemtoReg <= '0';
+				RegWrite <= '1';
+				MemRead <= '0';
+				MemWrite <= '0';
+				Branch <= '0';
 				ALUOp <= "011";
 			when "001100" => -- andi
-				RegDst <= "0";
-				ALUSrc <= "1";
-				MemtoReg <= "0";
-				RegWrite <= "1";
-				MemRead <= "0";
-				MemWrite <= "0";
-				Branch <= "0";
+				RegDst <= '0';
+				ALUSrc <= '1';
+				MemtoReg <= '0';
+				RegWrite <= '1';
+				MemRead <= '0';
+				MemWrite <= '0';
+				Branch <= '0';
 				ALUOp <= "100";
 			when "001101" => -- ori
-				RegDst <= "0";
-				ALUSrc <= "1";
-				MemtoReg <= "0";
-				RegWrite <= "1";
-				MemRead <= "0";
-				MemWrite <= "0";
-				Branch <= "0";
+				RegDst <= '0';
+				ALUSrc <= '1';
+				MemtoReg <= '0';
+				RegWrite <= '1';
+				MemRead <= '0';
+				MemWrite <= '0';
+				Branch <= '0';
 				ALUOp <= "101";
-			when "001101" => -- lui
-				RegDst <= "0";
-				ALUSrc <= "1";
-				MemtoReg <= "0";
-				RegWrite <= "1";
-				MemRead <= "0";
-				MemWrite <= "0";
-				Branch <= "0";
+			when "001111" => -- lui
+				RegDst <= '0';
+				ALUSrc <= '1';
+				MemtoReg <= '0';
+				RegWrite <= '1';
+				MemRead <= '0';
+				MemWrite <= '0';
+				Branch <= '0';
 				ALUOp <= "111";
             when others => -- otro caso
-                RegDst <= "0";
-				ALUSrc <= "0";
-				MemtoReg <= "0";
-				RegWrite <= "0";
-				MemRead <= "0";
-				MemWrite <= "0";
-				Branch <= "0";
+                RegDst <= '0';
+				ALUSrc <= '0';
+				MemtoReg <= '0';
+				RegWrite <= '0';
+				MemRead <= '0';
+				MemWrite <= '0';
+				Branch <= '0';
 				ALUOp <= "000";
 		end case;
 
@@ -267,21 +273,23 @@ begin
 	process(Clk,Reset)
 	begin
 		if (Reset = '1') then 
-            ID_RegDst <= "0";
-			ID_ALUSrc <= "0";
-			ID_MemtoReg <= "0";
-			ID_MemRead <= "0";
-			ID_MemWrite <= "0";
-			ID_Branch <= "0";
+            ID_RegDst <= '0';
+            ID_RegWrite <= '0';
+			ID_ALUSrc <= '0';
+			ID_MemtoReg <= '0';
+			ID_MemRead <= '0';
+			ID_MemWrite <= '0';
+			ID_Branch <= '0';
 			ID_ALUOp <= "000";
 			ID_pc_plus_4 <= x"00000000";
 			ID_read_data_1 <= x"00000000";
 			ID_read_data_2 <= x"00000000";
 			ID_signal_extend <= x"00000000";
-			ID_instruction_20_16 <= x"00000";
-			ID_instruction_15_11 <= x"00000";
+			ID_instruction_20_16 <= "00000";
+			ID_instruction_15_11 <= "00000";
 		elsif (rising_edge(Clk)) then
             ID_RegDst <= RegDst;
+            ID_RegWrite <= RegWrite;
 			ID_ALUSrc <= ALUSrc;
 			ID_MemtoReg <= MemtoReg;
 			ID_MemRead <= MemRead;
@@ -299,8 +307,8 @@ begin
 
 -- Declaracion de la ALU
 
-	mux_ALU <= ID_read_data_2 when (ID_ALUSrc = "0") else ID_signal_extend;
-	out_ALU_control <= "000"; --revisar
+	mux_ALU <= ID_read_data_2 when (ID_ALUSrc = '0') else ID_signal_extend;
+	--out_ALU_control <= "000";
 
 -- ALU control
 	process(ID_signal_extend, ID_ALUOp)
@@ -338,28 +346,32 @@ begin
 		result => result_ALU
 		);
 
-	add_jump <= (ID_signal_extend(29 downto 0) & "00") + ID_pc_plus_4;
+	EX_pc_branch <= (ID_signal_extend(29 downto 0) & "00") + ID_pc_plus_4;
 
-	mux_instruction <= ID_instruction_20_16 when (ID_RegDst = "0") else ID_instruction_15_11;
+	mux_instruction <= ID_instruction_20_16 when (ID_RegDst = '0') else ID_instruction_15_11;
+
+
 -- Registro de segmentacion EX/MEM
 	process(Clk,Reset)
 	begin
 		if (Reset = '1') then 
-			EX_MemtoReg <= "0";
-			EX_MemRead <= "0";
-			EX_MemWrite <= "0";
-			EX_Branch <= "0";
-			EX_add_jump <= x"00000000";
+			EX_MemtoReg <= '0';
+			EX_MemRead <= '0';
+			EX_MemWrite <= '0';
+            EX_RegWrite <= '0';
+			EX_Branch <= '0';
+			pc_branch <= x"00000000";
 			EX_zero_ALU <= '0';
 			EX_result_ALU <= x"00000000";
 			EX_read_data_2 <= x"00000000";
-			EX_mux_instruction <= x"00000";
+			EX_mux_instruction <= "00000";
 		elsif (rising_edge(Clk)) then
 			EX_MemtoReg <= ID_MemtoReg;
 			EX_MemRead <= ID_MemRead;
 			EX_MemWrite <= ID_MemWrite;
+            EX_RegWrite <= ID_RegWrite;
 			EX_Branch <= ID_Branch;
-			EX_add_jump <= add_jump;
+			pc_branch <= EX_pc_branch;
 			EX_zero_ALU <= zero_ALU;
 			EX_result_ALU <= result_ALU;
 			EX_read_data_2 <= ID_read_data_2;
@@ -367,5 +379,39 @@ begin
 		end if;
 	end process;
 	
+-- Etapa MEM
+ 
+pc_src <= EX_zero_ALU and EX_Branch; 
+
+    D_Addr <= EX_result_ALU;
+	D_RdStb <= EX_MemRead;
+	D_WrStb <= EX_MemWrite;
+	D_DataOut <= EX_read_data_2;
+    -- D_DataIn contiene el valor
+
+-- Registro de segmentacion EX/MEM
+	process(Clk,Reset)
+	begin
+		if (Reset = '1') then 
+			WB_MemtoReg <= '0';
+            WB_RegWrite <= '0';
+			WB_write_register <= "00000";
+			WB_read_data_2 <= x"00000000";
+			WB_result_data_memory <= x"00000000";
+		elsif (rising_edge(Clk)) then
+			WB_MemtoReg <= EX_MemtoReg;
+            WB_RegWrite <= EX_RegWrite;
+			WB_result_data_memory <= D_DataIn;
+			WB_read_data_2 <= EX_read_data_2;
+			WB_write_register <= EX_mux_instruction;
+		end if;
+	end process;
+
+-- Etapa WB
+
+WB_write_data <= WB_result_data_memory when (WB_MemtoReg = '0') else WB_read_data_2;
+
+
+
 
 end processor_arq;
